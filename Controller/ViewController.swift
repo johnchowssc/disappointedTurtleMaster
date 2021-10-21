b7435fa332eb59176b6f1780eedd13af13f4cf72
johnchowssc <71998398+johnchowssc@users.noreply.github.com>
2021-10-21 17:59:23 +1100

    Refactored code to MVC pattern

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -7,27 +7,20 @@
 
 import UIKit
 
-let turtleStatuses = [
-"disappointed in you.",
-"very disappointed in you.",
-"dismayed by you.",
-"saddened by your presence.",
-"grieving over the time wasted on you.",
-"in a deep funk."
-]
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var status = TurtleStatuses() //Initialise instance of TurtleStatuses structure for use in this view.
 
     @IBOutlet weak var turtleStatusLabel: UILabel!
     
     @IBAction func turtleStatusButton(_ sender: UIButton) {
-        let index = Int.random(in: 0..<turtleStatuses.count)
-        turtleStatusLabel.text = turtleStatuses[index]
+        let index = Int.random(in: 0..<status.turtleStatuses.count)
+        turtleStatusLabel.text = status.turtleStatuses[index]
     }
 }
 
---
+++ Model/TurtleStatuses.swift
@@ -0,0 +1,20 @@
+//
+//  TurtleStatuses.swift
+//  disappointedTurtleMaster
+//
+//  Created by John Chow on 21/10/21.
+//
+
+import Foundation
+
+//Structure to contain all of the turtle statuses.
+struct TurtleStatuses {
+    let turtleStatuses = [
+    "disappointed in you.",
+    "very disappointed in you.",
+    "dismayed by you.",
+    "saddened by your presence.",
+    "grieving over the time wasted on you.",
+    "in a deep funk."
+    ]
+}
